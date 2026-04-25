a152f15b73b5e7425fef16bdbcecb3cd3b0241ac
Marcel Parizzi <marcelparizzi@gmail.com>
2026-04-25 02:32:18 -0300

    corrige parametros

--- app/src/config/env.ts
+++ app/src/config/env.ts
@@ -31,7 +31,7 @@ export function initGoogleAnalytics(): void {
   // Initialize gtag
   (window as any).dataLayer = (window as any).dataLayer || [];
   function gtag(...args: any[]) {
-    (window as any).dataLayer.push(arguments);
+    (window as any).dataLayer.push(args);
   }
   (window as any).gtag = gtag;
   
